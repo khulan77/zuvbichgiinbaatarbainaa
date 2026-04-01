b47bc2cf858918a20ad7a58557b4aceab10d2de2
25LP4490 25LP4490 <altsanselemmungunerdne@gmaile.com>
2026-04-01 11:06:46 +0800

    login section

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from "next";
-import { Lexend, Lora } from "next/font/google"; 
+import { Lexend, Lora } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
+// 1. ClerkProvider-ийг импортлох
+import { ClerkProvider } from "@clerk/nextjs";
 
 const lexend = Lexend({
   variable: "--font-lexend",
-  subsets: ["latin"] as any, 
+  subsets: ["latin"] as any,
   weight: ["300", "400", "500", "700"],
-  display: "swap", 
+  display: "swap",
 });
 
 const lora = Lora({
@@ -30,13 +32,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="mn">
-      <head />
-      <body className={`${lexend.variable} ${lora.variable} font-sans antialiased min-h-screen flex flex-col`}> 
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="mn">
+        <head />
+        <body
+          className={`${lexend.variable} ${lora.variable} font-sans antialiased min-h-screen flex flex-col`}
+        >
+          <Header />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+    ],
+  },
+  // Таны кодонд Turbopack ашиглагдаж байгаа бол:
+  experimental: {
+    turbo: {
+      // Турбо тохиргоонууд энд орно
+    },
+  },
 };
 
 export default nextConfig;
